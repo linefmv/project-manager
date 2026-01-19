3538c4927b19f26a0ed82e5c204995c377c9aee7
Aline Viana <alinemaciel018@gmail.com>
2026-01-19 14:03:35 -0300

    chore: adjust port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
     base: command === 'build' ? '/project-manager/' : '/',
     plugins: [react()],
+    server: {
+        port: 3000,
+    },
     test: {
         globals: true,
         environment: 'jsdom',
