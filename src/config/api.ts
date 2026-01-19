dd87bf184a7f05cb94e413d129e48b73e780c409
Aline Viana <alinemaciel018@gmail.com>
2026-01-18 21:06:28 -0300

    ci: improve deploy setup

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL = 'https://us-central1-backend-project-manager.cloudfunctions.net/api'
 
 export const API_ENDPOINTS = {
     projects: '/projects',
